b952444981ef62614cbf6348b34aeedb8d9530f4
haozai309 <haozhanghao_87@163.com>
2014-12-30 21:07:21 +0800

    script related to introduction of ruby

---
+++ begin.rb
@@ -0,0 +1,36 @@
+
+
+# begin of learning ruby
+puts "haozai309".length
+puts "haozai309".reverse
+puts "haozai309".upcase
+puts "haozai309".downcase
+
+
+=begin
+Multi line comment
+no need any #
+=end
+
+my_name = "haozai309"
+my_age = 27
+
+
+# a little math
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+
+# string & method
+name = "haozai309".downcase
+name.reverse.upcase
+
+
+#--------------------------------------
+
+
+
+
+
+
